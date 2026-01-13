196d4c52e532f87ab690198d5880d38a50f08ab3
zzc <zzcqiyue@gmail.com>
2026-01-14 01:59:07 +0800

    初始化项目基本命令

--- main.py
+++ main.py
@@ -2,23 +2,68 @@ from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("astrbot_plugin_wot", "zzc", "查询坦克世界效率和战绩", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
-    
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
-    async def terminate(self):
+    @filter.command("wot帮助")
+    async def wot_command_collection(self, event: AstrMessageEvent):
+        """获取命令合集"""
+        user_name = event.get_sender_name()
+        message_str = event.message_str
+        message_chain = event.get_messages()
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
+
+    @filter.command("今日效率",alias={"今日战绩"})
+    async def get_today_performance(self, event: AstrMessageEvent):
+        """查询今日效率"""
+        user_name = event.get_sender_name()
+        message_str = event.message_str
+        message_chain = event.get_messages()
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
+
+    @filter.command("昨日效率",alias={"昨日战绩"})
+    async def get_yesterday_performance(self, event: AstrMessageEvent):
+        """查询昨日效率"""
+        user_name = event.get_sender_name()
+        message_str = event.message_str
+        message_chain = event.get_messages()
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
+
+    @filter.command("两日效率", alias={"两日战绩"})
+    async def get_two_days_performance(self, event: AstrMessageEvent):
+        """查询两日效率"""
+        user_name = event.get_sender_name()
+        message_str = event.message_str
+        message_chain = event.get_messages()
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
+
+    @filter.command("三日效率", alias={"三日战绩"})
+    async def get_three_days_performance(self, event: AstrMessageEvent):
+        """查询三日效率"""
+        user_name = event.get_sender_name()
+        message_str = event.message_str
+        message_chain = event.get_messages()
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
+
+    @filter.command("百场效率", alias={"百场战绩"})
+    async def get_hundred_matches_performance(self, event: AstrMessageEvent):
+        """查询百场效率"""
+        user_name = event.get_sender_name()
+        message_str = event.message_str
+        message_chain = event.get_messages()
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")
+    
+
+async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
